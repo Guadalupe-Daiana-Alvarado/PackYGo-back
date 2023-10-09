70b0555a464161c68a9667e23a98fdcb6a97c858
Benja0420 <benjaminsepulveda.0420@gmail.com>
2023-10-09 17:53:37 -0300

    bcriptjs

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ import express from  'express';
 import signUp from '../controller/register/register.js';
 import validator from '../middlewares/validator.js';
 import userSchema from "../schema/userSchema.js";
+import hasheador from "../middlewares/hasheador.js";
 let router = express.Router();
 
-router.post('/register',validator(userSchema), signUp);
+router.post('/register',validator(userSchema),hasheador , signUp);
 
 export default router;
