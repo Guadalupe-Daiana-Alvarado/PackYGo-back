e6b04990c5cc175a346c87ccdac466260cfa8ebe
Guadalupe-Alvarado <dai.22121994@gmail.com>
2023-10-10 17:44:59 -0300

    agregado de ruta y controlador de categories

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import productsRouter from './products.js';
 import UserRouter from "./users.js"
+import categoriesRouter from './categories.js'
+
 let router = express.Router();
 
 router.use('/products', productsRouter)
 
 router.use('/users', UserRouter);
+
+router.use('/categories',categoriesRouter)
 
 export default router;
