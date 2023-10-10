3913562154de5fd315d9e00134c53d18f3b0a97c
Benja0420 <benjaminsepulveda.0420@gmail.com>
2023-10-10 12:19:16 -0300

    cors

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 import 'dotenv/config.js';
 import './config/database.js'
+import cors from 'cors';
 
 let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
