29e9f45bf718233e632b4fe6044e499408a1e2a6
Guadalupe-Alvarado <dai.22121994@gmail.com>
2023-10-10 21:54:51 -0300

    Intentando los check

--- controllers/products/productController.js
+++ controllers/products/productController.js
@@ -3,14 +3,16 @@ import Product from '../../models/Product.js';
 export const getAllProducts = async (req, res) => {
   try {
     let filters = {}; 
+
     
     if (req.query.name) { 
       console.log('REQEEEE',req.query.name)
       filters.name = new RegExp(req.query.name, 'i'); 
     }
-
+    if (req.query.category) filters['category_id'] = req.query.category.split(",");
+   console.log('holiiiii',req.query._id)
     // Consulta los productos con los filtros aplicados
-    const products = await Product.find(filters);
+    const products = await Product.find(filters).populate("category", "name");
 
     res.status(200).json({
       products: products
