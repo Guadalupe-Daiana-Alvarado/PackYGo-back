5b0819d3fa56e4b2af55452583bc33557c532b9b
Guadalupe-Alvarado <dai.22121994@gmail.com>
2023-10-16 01:03:03 -0300

    mama

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 import express from  'express';
 import signUp from '../controllers/register/register.js';
+import signout from '../controllers/login/logout.js';
 import validator from '../middlewares/validator.js';
 import userSchema from "../schema/userSchema.js";
 import hasheador from "../middlewares/hasheador.js";
@@ -8,9 +9,14 @@ import passwordBcript from '../middlewares/passwordBcript.js';
 import accountNotExist from '../middlewares/accountNotExist.js';
 import generateToken from '../middlewares/generateToken.js';
 import { requireAuth } from '../middlewares/authUser.js';
+
+
+
 let router = express.Router();
 
 router.post('/register',validator(userSchema),hasheador , signUp);
-router.post('/login', accountNotExist, passwordBcript, generateToken,signIn,);
+router.post('/login', accountNotExist, passwordBcript, generateToken, signIn,);
+router.post('/signout', signout)
+
 
 export default router;
