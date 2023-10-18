7a75b77448d28edd3daa1d0f0f483ec8f6346a42
Guadalupe-Alvarado <dai.22121994@gmail.com>
2023-10-18 00:22:38 -0300

    controlador de leer usuario por id

--- controllers/register/register.js
+++ controllers/register/register.js
@@ -12,3 +12,33 @@ const signUp = async (req, res) => {
 };
 
 export default signUp;
+
+
+/* import User from "../../models/User.js";
+
+const signUp = async (req, res) => {
+  const { email, password, photo, name, surname, address, phone } = req.body;
+
+  try {
+    // Intenta crear un nuevo usuario
+    const user = await User.create({
+      email,
+      password,
+      photo,
+      name,
+      surname,
+      address,
+      phone,
+    });
+
+    // Si la creación es exitosa, responde con un código 201 (Created) y los datos del usuario
+    res.status(201).json({ user });
+  } catch (error) {
+    // Si se produce un error, registra el error y responde con un código 500 (Internal Server Error)
+    console.error("Error al crear un nuevo usuario:", error);
+    res.status(500).json({ error: "Error al crear un nuevo usuario" });
+  }
+};
+
+export default signUp;
+ */
